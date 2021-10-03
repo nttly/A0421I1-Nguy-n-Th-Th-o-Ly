f7a59088afed48f40c4edc4e84d5db361cd8bf69
Your Name <you@example.com>
2021-10-04 03:19:13 +0700

    nopbai

--- module3/bai2_thiet_ke_va_tao_CSDL/baitap/baitap2/quan_ly_ban_hang.sql
+++ module3/bai2_thiet_ke_va_tao_CSDL/baitap/baitap2/quan_ly_ban_hang.sql
@@ -1,5 +1,5 @@
-create database quan_ly_ban_hang3;
-use quan_ly_ban_hang3;
+create database quan_ly_ban_hang2;
+use quan_ly_ban_hang2;
 create table customer(
 	id int auto_increment primary key,
     `name` varchar(50),
@@ -13,6 +13,12 @@ create table `Order`(
     id_product int,
     foreign key(id_product) references customer(id)
 );
+
+create table product(
+	id int auto_increment primary key,
+    `name` varchar(50),
+    price double
+);
 create table OrderDetail(
 	id_order int ,
     id_product int,
@@ -20,9 +26,4 @@ create table OrderDetail(
     constraint pk_oder_product primary key(id_order,id_product ),
     foreign key (id_order) references `Order`(id),
     foreign key (id_product) references product(id)
-);
-create table product(
-	id int auto_increment primary key,
-    `name` varchar(50),
-    price double
 );
